f489279771d92e359eb7d7767e405d124b27ea50
Peter Karalekas <peter@karalekas.com>
2023-12-08 01:06:16 -0800

    Add a dryad expansion test for a barbell of 2 3-blossoms

--- tests/dryad.lisp
+++ tests/dryad.lisp
@@ -46,6 +46,68 @@
              (D :id (id 6) :match-edge (vv-edge D C)))
           (is (tree-equalp original-tree target-tree)))))))
 
+(deftest test-dryad-expand-barbell-3-blossom-x2 ()
+  "Tests that a DRYAD will properly execute the following blossom expansion:
+
+        +                   +
+     BLOSSOM             BLOSSOM2                +     +     +     +     +     +
+ [A --- B --- C] === [D --- E --- F]     -->     A === B     C === D     E === F
+ [\-----------/]     [\-----------/]
+"
+  (with-with ((with-courier ())
+              (with-simulation (simulation (*local-courier*))))
+    (let* ((dryad (spawn-process 'dryad :match-address (register)
+                                        :debug? t))
+           (dryad-address (process-public-address dryad)))
+      (simulation-add-event simulation (make-event :callback dryad))
+      (blossom-let (original-tree :dryad dryad-address)
+          ((BLOSSOM :id (gensym "BLOSSOM-A")
+                    :match-edge (bb-edge BLOSSOM C D BLOSSOM2)
+                    :petals (list (vv-edge A C)
+                                  (vv-edge C B)
+                                  (vv-edge B A)))
+           (BLOSSOM2 :id (gensym "BLOSSOM-B")
+                     :match-edge (bb-edge BLOSSOM2 D C BLOSSOM)
+                     :petals (list (vv-edge D F)
+                                   (vv-edge F E)
+                                   (vv-edge E D)))
+           (A :id (id 0) :internal-weight 2 :pistil BLOSSOM)
+           (B :id (id 2) :pistil BLOSSOM)
+           (C :id (id 4) :internal-weight 2 :pistil BLOSSOM)
+           (D :id (id 6) :pistil BLOSSOM2)
+           (E :id (id 8) :internal-weight 2 :pistil BLOSSOM2)
+           (F :id (id 10) :pistil BLOSSOM2)
+           )
+        (simulate-add-tree simulation original-tree :dryad dryad)
+        (simulate-until-dead simulation BLOSSOM :timeout 100)
+        (simulate-until-dead simulation BLOSSOM2 :timeout 100)
+        (blossom-let (target-tree :dryad dryad-address)
+            ((BLOSSOM-new :id (slot-value BLOSSOM 'anatevka::id)
+                          :match-edge (bb-edge BLOSSOM-new C D D)
+                          :petals (list (vv-edge A C)
+                                        (vv-edge C B)
+                                        (vv-edge B A))
+                          :wilting t)
+             ;; nb: the match-edge here still has the other macrovertex in it
+             ;;     which probably means that BLOSSOM2 was popped first
+             (BLOSSOM2-new :id (slot-value BLOSSOM2 'anatevka::id)
+                           :match-edge (bb-edge BLOSSOM2-new D C BLOSSOM-new)
+                           :petals (list (vv-edge D F)
+                                         (vv-edge F E)
+                                         (vv-edge E D))
+                           :wilting t
+                           )
+             (A :id (id 0) :match-edge (vv-edge A B)
+                :internal-weight 2)
+             (B :id (id 2) :match-edge (vv-edge B A))
+             (C :id (id 4) :match-edge (vv-edge C D)
+                :internal-weight 2)
+             (D :id (id 6) :match-edge (vv-edge D C))
+             (E :id (id 8) :internal-weight 2 :match-edge (vv-edge E F))
+             (F :id (id 10) :match-edge (vv-edge F E))
+             )
+          (is (tree-equalp original-tree target-tree)))))))
+
 (deftest test-dryad-expand-barbell-5-blossom ()
   "Tests that a DRYAD will properly execute the following blossom expansion:
 

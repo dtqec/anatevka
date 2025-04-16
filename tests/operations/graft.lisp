2f64d21656e434534990f7302b9b678913f5e112
Peter Karalekas <peter@karalekas.com>
2025-04-15 21:52:56 -0700

    Add a test that grafts a macrovertex into a negative position

--- tests/operations/graft.lisp
+++ tests/operations/graft.lisp
@@ -37,3 +37,60 @@ A ~~~ B == C       -->      A --> B ==> C"
                          :match-edge (vv-edge outer-node inner-node)
                          :parent (vv-edge outer-node inner-node)))
           (is (tree-equalp original-tree target-tree)))))))
+
+(deftest test-supervisor-graft-inner-3-blossom ()
+  "Checks the transformation
+
+              +                            -
+           BLOSSOM      +               BLOSSOM      +
+          [      D] === E              [      D] ==> E
+          [    / |]         -->        [    / |]
+          [  /   |]                    [  /   |]
+    A     [B --- C]              A --> [B --- C]
+    +      +     +               +      -     -
+"
+  (with-with ((with-courier ())
+              (with-simulation (simulation (*local-courier*))))
+    (let ((dryad-address (register)))
+      (blossom-let (original-tree :dryad dryad-address)
+          ((A :id (id 0) :paused? T)
+           (B :id (id 2) :pistil BLOSSOM
+              :internal-weight 2)
+           (C :id (id 4) :pistil BLOSSOM)
+           (D :id (id 4 2) :pistil BLOSSOM
+              :internal-weight 2)
+           (E :id (id 6 2)
+              :match-edge (bb-edge E E D BLOSSOM))
+           (BLOSSOM :id 'blossom
+                    :petals (list (vv-edge B C)
+                                  (vv-edge C D)
+                                  (vv-edge D B))
+                    :match-edge (bb-edge BLOSSOM D E E)))
+        (let ((supervisor (supervisor simulation
+                                      :edges (list (bb-edge BLOSSOM D E E)
+                                                   (bb-edge A A B BLOSSOM))
+                                      :source-root (process-public-address A)
+                                      :target-root (process-public-address BLOSSOM)
+                                      :recommendation ':graft
+                                      :source-id (id 0))))
+          (simulate-add-tree simulation original-tree)
+          (simulate-until-dead simulation supervisor)
+          (blossom-let (target-tree :dryad dryad-address)
+              ((A :id (id 0) :children (list (bb-edge A A B BLOSSOM)))
+               (B :id (id 2) :pistil BLOSSOM
+                  :internal-weight 2)
+               (C :id (id 4) :pistil BLOSSOM)
+               (D :id (id 4 2) :pistil BLOSSOM
+                  :internal-weight 2)
+               (E :id (id 6 2)
+                  :parent (bb-edge E E D BLOSSOM)
+                  :match-edge (bb-edge E E D BLOSSOM))
+               (BLOSSOM :id 'blossom
+                        :petals (list (vv-edge B C)
+                                      (vv-edge C D)
+                                      (vv-edge D B))
+                        :parent (bb-edge BLOSSOM B A A)
+                        :match-edge (bb-edge BLOSSOM D E E)
+                        :positive? nil
+                        :children (list (bb-edge BLOSSOM D E E))))
+            (is (tree-equalp original-tree target-tree))))))))
